e2bd15e8b5a6ed6466966c7351f4a97dea170722
MB0026 <maxim.butrimov@gmail.com>
2026-03-17 13:33:26 +0100

    Add AnimatedTitle component with greeting message

---
+++ AnimatedTitle/Code.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AnimatedTitle.css";
+
+const AnimatedTitle = () => {
+  return (
+    <h1 className="animated-title">
+      👋 Hola, soy Maxim Butrimov
+    </h1>
+  );
+};
+
+export default AnimatedTitle;
